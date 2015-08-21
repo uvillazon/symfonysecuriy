f278eda80615be4675ef57134f66732c0847ae20
Ubaldo Francisco Villazon Villca <ubaldo.villazon@elfec.bo>
2015-08-21 14:22:28 -0400

    implementando las opciones de mnenu relacionados los botones

--- SGAUTH/src/Elfec/SgauthBundle/Entity/botones.php
+++ SGAUTH/src/Elfec/SgauthBundle/Entity/botones.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
  * Elfec.botones
  *
  * @ORM\Table(name="elfec.botones", indexes={@ORM\Index(name="IDX_2E1E485B3DE02BDB", columns={"id_padre"}), @ORM\Index(name="IDX_2E1E485BD9EAF441", columns={"id_opc"})})
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="Elfec\SgauthBundle\Entity\Repository\botonesRepository")
  */
 class botones
 {
@@ -79,38 +79,323 @@ class botones
     private $estado;
 
     /**
-     * @var \Elfec.botones
+     * @var \botones
      *
-     * @ORM\ManyToOne(targetEntity="Elfec.botones")
+     * @ORM\ManyToOne(targetEntity="botones")
      * @ORM\JoinColumns({
      *   @ORM\JoinColumn(name="id_padre", referencedColumnName="id_boton")
      * })
      */
-    private $idPadre;
+    private $padre;
 
     /**
-     * @var \Elfec.menuOpciones
+     * @var \menuOpciones
      *
-     * @ORM\ManyToOne(targetEntity="Elfec.menuOpciones")
+     * @ORM\ManyToOne(targetEntity="menuOpciones")
      * @ORM\JoinColumns({
      *   @ORM\JoinColumn(name="id_opc", referencedColumnName="id_opc")
      * })
      */
+    private $menuOpciones;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="id_padre", type="decimal", precision=10, scale=0, nullable=false)
+     */
+    private $idPadre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="id_opc", type="decimal", precision=10, scale=0, nullable=false)
+     */
     private $idOpc;
 
-    /**
-     * @var \Doctrine\Common\Collections\Collection
-     *
-     * @ORM\ManyToMany(targetEntity="Elfec.perfiles", mappedBy="idBoton")
-     */
-    private $idPerfil;
 
     /**
-     * Constructor
+     * Get idBoton
+     *
+     * @return string
      */
-    public function __construct()
+    public function getIdBoton()
     {
-        $this->idPerfil = new \Doctrine\Common\Collections\ArrayCollection();
+        return $this->idBoton;
     }
 
+    /**
+     * Set boton
+     *
+     * @param string $boton
+     * @return botones
+     */
+    public function setBoton($boton)
+    {
+        $this->boton = $boton;
+
+        return $this;
+    }
+
+    /**
+     * Get boton
+     *
+     * @return string
+     */
+    public function getBoton()
+    {
+        return $this->boton;
+    }
+
+    /**
+     * Set tooltip
+     *
+     * @param string $tooltip
+     * @return botones
+     */
+    public function setTooltip($tooltip)
+    {
+        $this->tooltip = $tooltip;
+
+        return $this;
+    }
+
+    /**
+     * Get tooltip
+     *
+     * @return string
+     */
+    public function getTooltip()
+    {
+        return $this->tooltip;
+    }
+
+    /**
+     * Set idItem
+     *
+     * @param string $idItem
+     * @return botones
+     */
+    public function setIdItem($idItem)
+    {
+        $this->idItem = $idItem;
+
+        return $this;
+    }
+
+    /**
+     * Get idItem
+     *
+     * @return string
+     */
+    public function getIdItem()
+    {
+        return $this->idItem;
+    }
+
+    /**
+     * Set estilo
+     *
+     * @param string $estilo
+     * @return botones
+     */
+    public function setEstilo($estilo)
+    {
+        $this->estilo = $estilo;
+
+        return $this;
+    }
+
+    /**
+     * Get estilo
+     *
+     * @return string
+     */
+    public function getEstilo()
+    {
+        return $this->estilo;
+    }
+
+    /**
+     * Set accion
+     *
+     * @param string $accion
+     * @return botones
+     */
+    public function setAccion($accion)
+    {
+        $this->accion = $accion;
+
+        return $this;
+    }
+
+    /**
+     * Get accion
+     *
+     * @return string
+     */
+    public function getAccion()
+    {
+        return $this->accion;
+    }
+
+    /**
+     * Set icono
+     *
+     * @param string $icono
+     * @return botones
+     */
+    public function setIcono($icono)
+    {
+        $this->icono = $icono;
+
+        return $this;
+    }
+
+    /**
+     * Get icono
+     *
+     * @return string
+     */
+    public function getIcono()
+    {
+        return $this->icono;
+    }
+
+    /**
+     * Set orden
+     *
+     * @param string $orden
+     * @return botones
+     */
+    public function setOrden($orden)
+    {
+        $this->orden = $orden;
+
+        return $this;
+    }
+
+    /**
+     * Get orden
+     *
+     * @return string
+     */
+    public function getOrden()
+    {
+        return $this->orden;
+    }
+
+    /**
+     * Set estado
+     *
+     * @param string $estado
+     * @return botones
+     */
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+
+        return $this;
+    }
+
+    /**
+     * Get estado
+     *
+     * @return string
+     */
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    /**
+     * Set idPadre
+     *
+     * @param string $idPadre
+     * @return botones
+     */
+    public function setIdPadre($idPadre)
+    {
+        $this->idPadre = $idPadre;
+
+        return $this;
+    }
+
+    /**
+     * Get idPadre
+     *
+     * @return string
+     */
+    public function getIdPadre()
+    {
+        return $this->idPadre;
+    }
+
+    /**
+     * Set idOpc
+     *
+     * @param string $idOpc
+     * @return botones
+     */
+    public function setIdOpc($idOpc)
+    {
+        $this->idOpc = $idOpc;
+
+        return $this;
+    }
+
+    /**
+     * Get idOpc
+     *
+     * @return string
+     */
+    public function getIdOpc()
+    {
+        return $this->idOpc;
+    }
+
+    /**
+     * Set padre
+     *
+     * @param \Elfec\SgauthBundle\Entity\botones $padre
+     * @return botones
+     */
+    public function setPadre(\Elfec\SgauthBundle\Entity\botones $padre = null)
+    {
+        $this->padre = $padre;
+
+        return $this;
+    }
+
+    /**
+     * Get padre
+     *
+     * @return \Elfec\SgauthBundle\Entity\botones
+     */
+    public function getPadre()
+    {
+        return $this->padre;
+    }
+
+    /**
+     * Set menuOpciones
+     *
+     * @param \Elfec\SgauthBundle\Entity\menuOpciones $menuOpciones
+     * @return botones
+     */
+    public function setMenuOpciones(\Elfec\SgauthBundle\Entity\menuOpciones $menuOpciones = null)
+    {
+        $this->menuOpciones = $menuOpciones;
+
+        return $this;
+    }
+
+    /**
+     * Get menuOpciones
+     *
+     * @return \Elfec\SgauthBundle\Entity\menuOpciones
+     */
+    public function getMenuOpciones()
+    {
+        return $this->menuOpciones;
+    }
 }
