a44a0346f953690b7f363d137382437713c2dd06
unknown <eballesteros@ELFEC1157.elfec.com>
2015-10-05 10:10:24 -0400

    creacion de un nuevo bundle

--- SGAUTH/app/AppKernel.php
+++ SGAUTH/app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Elfec\SgauthBundle\ElfecSgauthBundle(),
+            new Elfec\ReportesBundle\ElfecReportesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ SGAUTH/src/Elfec/ReportesBundle/ElfecReportesBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Elfec\ReportesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ElfecReportesBundle extends Bundle
+{
+}
