999668a4c907b1bf157e124161a252832e521b32
Ubaldo Francisco Villazon Villca <ubaldo.villazon@elfec.bo>
2016-05-16 16:46:52 -0400

    arbol de opciones de menu

--- SGAUTH/web/app/view/Opciones/GridOpciones.js
+++ SGAUTH/web/app/view/Opciones/GridOpciones.js
@@ -2,6 +2,7 @@
  * Created by uvillazon on 30/07/2015.
  */
 Ext.define("App.View.Opciones.GridOpciones", {
+    // extend: "Ext.tree.Panel",
     extend: "App.Config.Abstract.Grid",
     alias: 'widget.gridOpciones',
     title: 'Opciones de Menu',
@@ -11,10 +12,66 @@ Ext.define("App.View.Opciones.GridOpciones", {
     tabla: 'menu_opciones',
     id_tabla: 'id_opc',
     classStore: 'App.Store.Opciones.Opciones',
+    // useArrows: true,
+    // rootVisible: false,
+    // multiSelect: true,
     //parametros obligados para mostrar reporte de historico de estados por tabla
     initComponent: function () {
         var me = this;
+        // Ext.define('Task', {
+        //     extend: 'Ext.data.TreeModel',
+        //     fields: [
+        //         {name: 'task',     type: 'string'},
+        //         {name: 'user',     type: 'string'},
+        //         {name: 'duration', type: 'string'},
+        //         {name: 'done',     type: 'boolean'}
+        //     ]
+        // });
+        // var store = Ext.create('Ext.data.TreeStore', {
+        //     model: 'Task',
+        //     proxy: {
+        //         type: 'ajax',
+        //         //the store will get the content from the .json file
+        //         url: 'treegrid.js'
+        //     },
+        //     folderSort: true
+        // });
         me.store = Ext.create(me.classStore);
+        // me.store = store;
+        // me.columns =  [{
+        //     xtype: 'treecolumn', //this is so we know which column will show the tree
+        //     text: 'Task',
+        //     width: 200,
+        //     sortable: true,
+        //     dataIndex: 'task',
+        //     locked: true
+        // }, {
+        //     text: 'Assigned To',
+        //     width: 150,
+        //     dataIndex: 'user',
+        //     sortable: true
+        // }, {
+        //     xtype: 'checkcolumn',
+        //     header: 'Done',
+        //     dataIndex: 'done',
+        //     width: 40,
+        //     stopSelection: false
+        // }, {
+        //     text: 'Edit',
+        //     width: 40,
+        //     menuDisabled: true,
+        //     xtype: 'actioncolumn',
+        //     tooltip: 'Edit task',
+        //     align: 'center',
+        //     icon: '../simple-tasks/resources/images/edit_task.png',
+        //     handler: function(grid, rowIndex, colIndex, actionItem, event, record, row) {
+        //         Ext.Msg.alert('Editing' + (record.get('done') ? ' completed task' : '') , record.get('task'));
+        //     },
+        //     // Only leaf level tasks may be edited
+        //     isDisabled: function(view, rowIdx, colIdx, item, record) {
+        //         return !record.data.leaf;
+        //     }
+        // }];
 
         me.CargarComponentes();
         me.columns = [
